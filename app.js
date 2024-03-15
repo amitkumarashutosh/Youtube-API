bb398d04eaa1a2f4a98ed2abbc897d856eda0ba0
Amit <amitkumarashutos@gmail.com>
2024-03-15 23:55:58 +0530

    setup app.js file

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ import express from "express";
 const app = express();
 const port = process.env.port || 5000;
 
+//rest of the packages
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("connect to db successfully");
 });
